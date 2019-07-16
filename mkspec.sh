e9f3aa5d5f2e54027658f6bb5c81e7ac46413f4c
David Woodhouse <dwmw2@infradead.org>
2019-07-16 14:22:42 +0100

    Support autobuild for COPR
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

---
+++ mkspec.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GITCOMMIT="$(git rev-parse HEAD)"
+GITDESC="$(git describe --tags HEAD)"
+
+GITTAG="$(echo $GITDESC | cut -f1 -d-)"
+GITTAG="${GITTAG#v}"
+
+if [ "v$GITTAG" = "$GITDESC" ]; then
+    ISSNAP=0
+    GITCOUNT=0
+else
+    ISSNAP=1
+    GITCOUNT="$(echo $GITDESC | cut -f2 -d-)"
+fi
+
+sed -e "s/@ISSNAP@/${ISSNAP}/" \
+    -e "s/@VERSION@/${GITTAG}/" \
+    -e "s/@SNAPCOMMIT@/${GITCOMMIT}/" \
+    -e "s/@SNAPCOUNT@/${GITCOUNT}/" \
+    openconnect.spec.in > openconnect.spec
