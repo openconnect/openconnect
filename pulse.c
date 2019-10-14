openconnect/openconnect

commit 097586feac0c625896ac4a75ddf4f58120a0463d

Fix leaks in Pulse duplicate session handling
Clean up handling of from/sessid strings in processing session lists.

Spotted by Coverity.

Signed-off-by: David Woodhouse <dwmw2@infradead.org>

pulse.c
@@ -922,6 +922,8 @@ static int pulse_request_session_kill(struct openconnect_info *vpninfo, struct o
 		if (parse_avp(vpninfo, &p, &l, &avp_p, &avp_len, &avp_flags,
 			      &avp_vendor, &avp_code)) {
 		badlist:
+			free(from);
+			free(sessid);
 			vpn_progress(vpninfo, PRG_ERR,
 				     _("Failed to parse session list\n"));
 			ret = -EINVAL;
@@ -939,8 +941,10 @@ static int pulse_request_session_kill(struct openconnect_info *vpninfo, struct o
 			dump_avp(vpninfo, avp_flags, avp_vendor, avp_code, avp_p2, avp_len2);
 
 			if (avp_vendor == VENDOR_JUNIPER2 && avp_code == 0xd66) {
+				free(sessid);
 				sessid = strndup(avp_p2, avp_len2);
 			} else if (avp_vendor == VENDOR_JUNIPER2 && avp_code == 0xd67) {
+				free(from);
 				from = strndup(avp_p2, avp_len2);
 			} else if (avp_vendor == VENDOR_JUNIPER2 && avp_code == 0xd68 &&
 				   avp_len2 == 8) {
@@ -950,11 +954,8 @@ static int pulse_request_session_kill(struct openconnect_info *vpninfo, struct o
 			}
 		}
 
-		if (!from || !sessid || !when) {
-			free(from);
-			free(sessid);
+		if (!from || !sessid || !when)
 			goto badlist;
-		}
 
 		if (0
 #ifdef HAVE_LOCALTIME_S
@@ -970,6 +971,8 @@ static int pulse_request_session_kill(struct openconnect_info *vpninfo, struct o
 
 		buf_append(form_msg, " - %s from %s at %s\n", sessid, from, tmbuf);
 		free(from);
+		free(sessid);
+		from = sessid = NULL;
 		o.choices[i] = malloc(sizeof(struct oc_choice));
 		if (!o.choices[i]) {
 			ret = -ENOMEM;

commit a3e1d9d6badfc56a0231d199ddeb4af3baf0aaff

Fix pulse session kill request
Fixing leaks is good. Fixing them by freeing a string we were about to
use and then setting it to NULL, thus triggering a later check to report
-ENOMEM, less good.

Stupid dwmw2, no biscuit.

Fixes: 097586fe ("Fix leaks in Pulse duplicate session handling")

Signed-off-by: David Woodhouse <dwmw2@infradead.org>

pulse.c
@@ -972,19 +972,17 @@ static int pulse_request_session_kill(struct openconnect_info *vpninfo, struct o
 			snprintf(tmbuf, sizeof(tmbuf), "@%lu", (unsigned long)when);
 
 		buf_append(form_msg, " - %s from %s at %s\n", sessid, from, tmbuf);
+
 		free(from);
-		free(sessid);
-		from = sessid = NULL;
+		from = NULL;
+
 		o.choices[i] = malloc(sizeof(struct oc_choice));
 		if (!o.choices[i]) {
+			free(sessid);
 			ret = -ENOMEM;
 			goto out;
 		}
 		o.choices[i]->name = o.choices[i]->label = sessid;
-		if (!o.choices[i]->name) {
-			ret = -ENOMEM;
-			goto out;
-		}
 		i++;
 	}
 	ret = buf_error(form_msg);