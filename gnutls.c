openconnect/openconnect

commit a07183b79f231676bcfa9499738b93f5aa7d4937

Check select() return value in main loop
Coverity doesn't like it if we don't.

Signed-off-by: David Woodhouse <dwmw2@infradead.org>

mainloop.c
@@ -303,7 +303,12 @@ int openconnect_mainloop(struct openconnect_info *vpninfo,
 		tv.tv_sec = timeout / 1000;
 		tv.tv_usec = (timeout % 1000) * 1000;
 
-		select(vpninfo->_select_nfds, &rfds, &wfds, &efds, &tv);
+		if (select(vpninfo->_select_nfds, &rfds, &wfds, &efds, &tv) < 0) {
+			ret = -errno;
+			vpn_perror(vpninfo, "Failed select() in mainloop");
+			break;
+		}
+
 		if (vpninfo->tun_fd >= 0)
 			tun_r = FD_ISSET(vpninfo->tun_fd, &rfds);
 		if (vpninfo->dtls_fd >= 0)

commit a52d070d8d4fa1ba3ac723a4e350724ab7bfa22a

When select() returns with errno == EINTR, that isn't an error
This stopped us from actually sending the BYE packet and closing the
session cleanly on exit.

Fixes: a07183b79f ("Check select() return value in main loop") et al.

This is why we don't blindly "fix warnings" reported by tools like
Coverity, and should sometimes be a little more reticent, and only
actually fix *bugs* that are highlighted.

Signed-off-by: David Woodhouse <dwmw2@infradead.org>

gnutls.c
@@ -90,7 +90,8 @@ static int _openconnect_gnutls_write(gnutls_session_t ses, int fd, struct openco
 				FD_SET(fd, &rd_set);
 
 			cmd_fd_set(vpninfo, &rd_set, &maxfd);
-			if (select(maxfd + 1, &rd_set, &wr_set, NULL, NULL) < 0) {
+			if (select(maxfd + 1, &rd_set, &wr_set, NULL, NULL) < 0 &&
+			    errno != EINTR) {
 				vpn_perror(vpninfo, _("Failed select() for TLS"));
 				return -EIO;
 			}
@@ -144,7 +145,7 @@ static int _openconnect_gnutls_read(gnutls_session_t ses, int fd, struct opencon
 
 			cmd_fd_set(vpninfo, &rd_set, &maxfd);
 			ret = select(maxfd + 1, &rd_set, &wr_set, NULL, tv);
-			if (ret < 0) {
+			if (ret < 0 && errno != EINTR) {
 				vpn_perror(vpninfo, _("Failed select() for TLS"));
 				return -EIO;
 			}
@@ -240,7 +241,8 @@ static int openconnect_gnutls_gets(struct openconnect_info *vpninfo, char *buf, 
 				FD_SET(vpninfo->ssl_fd, &rd_set);
 
 			cmd_fd_set(vpninfo, &rd_set, &maxfd);
-			if (select(maxfd + 1, &rd_set, &wr_set, NULL, NULL) < 0) {
+			if (select(maxfd + 1, &rd_set, &wr_set, NULL, NULL) < 0 &&
+			    errno != EINTR) {
 				vpn_perror(vpninfo, _("Failed select() for TLS"));
 				return -EIO;
 			}
@@ -2293,7 +2295,8 @@ int cstp_handshake(struct openconnect_info *vpninfo, unsigned init)
 				FD_SET(ssl_sock, &rd_set);
 
 			cmd_fd_set(vpninfo, &rd_set, &maxfd);
-			if (select(maxfd + 1, &rd_set, &wr_set, NULL, NULL) < 0) {
+			if (select(maxfd + 1, &rd_set, &wr_set, NULL, NULL) < 0 &&
+			    errno != EINTR) {
 				vpn_perror(vpninfo, _("Failed select() for TLS"));
 				return -EIO;
 			}

mainloop.c
@@ -303,9 +303,11 @@ int openconnect_mainloop(struct openconnect_info *vpninfo,
 		tv.tv_sec = timeout / 1000;
 		tv.tv_usec = (timeout % 1000) * 1000;
 
-		if (select(vpninfo->_select_nfds, &rfds, &wfds, &efds, &tv) < 0) {
+		if (select(vpninfo->_select_nfds, &rfds, &wfds, &efds, &tv) < 0 &&
+		    errno != EINTR) {
 			ret = -errno;
-			vpn_perror(vpninfo, "Failed select() in mainloop");
+			vpn_perror(vpninfo, _("Failed select() in mainloop: %s"),
+				   strerror(-ret));
 			break;
 		}
 

ssl.c
@@ -106,7 +106,8 @@ static int cancellable_connect(struct openconnect_info *vpninfo, int sockfd,
 		FD_SET(sockfd, &ex_set);
 #endif
 		cmd_fd_set(vpninfo, &rd_set, &maxfd);
-		if (select(maxfd + 1, &rd_set, &wr_set, &ex_set, NULL) < 0) {
+		if (select(maxfd + 1, &rd_set, &wr_set, &ex_set, NULL) < 0 &&
+		    errno != EINTR) {
 			vpn_perror(vpninfo, _("Failed select() for socket connect"));
 			return -EIO;
 		}
@@ -839,7 +840,8 @@ void poll_cmd_fd(struct openconnect_info *vpninfo, int timeout)
 
 		FD_ZERO(&rd_set);
 		cmd_fd_set(vpninfo, &rd_set, &maxfd);
-		if (select(maxfd + 1, &rd_set, NULL, NULL, &tv) < 0) {
+		if (select(maxfd + 1, &rd_set, NULL, NULL, &tv) < 0 &&
+		    errno != EINTR) {
 			vpn_perror(vpninfo, _("Failed select() for command socket"));
 		}
 
@@ -1119,7 +1121,8 @@ int cancellable_send(struct openconnect_info *vpninfo, int fd,
 		FD_SET(fd, &wr_set);
 		cmd_fd_set(vpninfo, &rd_set, &maxfd);
 
-		if (select(maxfd + 1, &rd_set, &wr_set, NULL, NULL) < 0) {
+		if (select(maxfd + 1, &rd_set, &wr_set, NULL, NULL) < 0 &&
+		    errno != EINTR) {
 			vpn_perror(vpninfo, _("Failed select() for socket send"));
 			return -EIO;
 		}
@@ -1158,7 +1161,8 @@ int cancellable_recv(struct openconnect_info *vpninfo, int fd,
 		FD_SET(fd, &rd_set);
 		cmd_fd_set(vpninfo, &rd_set, &maxfd);
 
-		if (select(maxfd + 1, &rd_set, NULL, NULL, NULL) < 0) {
+		if (select(maxfd + 1, &rd_set, NULL, NULL, NULL) < 0 &&
+		    errno != EINTR) {
 			vpn_perror(vpninfo, _("Failed select() for socket recv"));
 			return -EIO;
 		}