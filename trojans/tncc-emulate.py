9e44786839beef50b3585563937edaefc8a909b8
Daniel Lenski <dlenski@gmail.com>
2020-04-04 17:20:23 -0700

    Add copyright and license notice, and update TNCC docs
    Signed-off-by: Daniel Lenski <dlenski@gmail.com>

--- trojans/tncc-emulate.py
+++ trojans/tncc-emulate.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Juniper/Pulse TNCC emulator
+#
+# Copyright © 2015-2018 Russ Dill
+#
+# Author: Russ Dill <russdill@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
 import sys
 import os
 import logging
