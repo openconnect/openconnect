openconnect/openconnect

commit 7cb8996e21b442c4ec60ce25c87e8a69516fac17

Clean up openconnect_protos handling
Do it with a bit less pointer arithmetic and it won't need a sentinel
at the end of the array.

It's a little bit cleaner, and hopefully Coverity will shut up about
leaks from openconnect_get_supported_protocols() potentially returning
non-negative after allocating the returned pointer.

Signed-off-by: David Woodhouse <dwmw2@infradead.org>

library.c
@@ -110,7 +110,7 @@ err:
 	return NULL;
 }
 
-const struct vpn_proto openconnect_protos[] = {
+static const struct vpn_proto openconnect_protos[] = {
 	{
 		.name = "anyconnect",
 		.pretty_name = N_("Cisco AnyConnect or openconnect"),
@@ -186,25 +186,26 @@ const struct vpn_proto openconnect_protos[] = {
 		.udp_catch_probe = oncp_esp_catch_probe,
 #endif
 	},
-	{ /* NULL */ }
 };
+
+#define NR_PROTOS (sizeof(openconnect_protos)/sizeof(*openconnect_protos))
 
 int openconnect_get_supported_protocols(struct oc_vpn_proto **protos)
 {
 	struct oc_vpn_proto *pr;
-	const struct vpn_proto *p;
+	int i;
 
-	*protos = pr = calloc(sizeof(openconnect_protos)/sizeof(*openconnect_protos), sizeof(*pr));
+	*protos = pr = calloc(NR_PROTOS, sizeof(*pr));
 	if (!pr)
 		return -ENOMEM;
 
-	for (p = openconnect_protos; p->name; p++, pr++) {
-		pr->name = p->name;
-		pr->pretty_name = _(p->pretty_name);
-		pr->description = _(p->description);
-		pr->flags = p->flags;
+	for (i = 0; i < NR_PROTOS; i++) {
+		pr[i].name = openconnect_protos[i].name;
+		pr[i].pretty_name = _(openconnect_protos[i].pretty_name);
+		pr[i].description = _(openconnect_protos[i].description);
+		pr[i].flags = openconnect_protos[i].flags;
 	}
-	return (p - openconnect_protos);
+	return i;
 }
 
 void openconnect_free_supported_protocols(struct oc_vpn_proto *protos)
@@ -220,8 +221,10 @@ const char *openconnect_get_protocol(struct openconnect_info *vpninfo)
 int openconnect_set_protocol(struct openconnect_info *vpninfo, const char *protocol)
 {
 	const struct vpn_proto *p;
+	int i;
 
-	for (p = openconnect_protos; p->name; p++) {
+	for (i = 0; i < NR_PROTOS; i++) {
+		p = &openconnect_protos[i];
 		if (strcasecmp(p->name, protocol))
 			continue;
 		vpninfo->proto = p;

commit ca8ec5e453929bb7a8cb1e5e7a52e49a6bd10232

Fix print_supported_protocols and print_supported_protocols_usage
These were broken in 7cb8996e21b442c4ec60ce25c87e8a69516fac17, when the
empty sentinel value at the end of the array was removed, without changing
the way these functions iterate over that array.

For some reason, this continues to work on Linux (probably due to `calloc`
allocating more zeroed bytes than we request, in
`openconnect_get_supported_protocols`), but is causing the expected SIGSEGV on
Solaris:
https://lists.infradead.org/pipermail/openconnect-devel/2020-April/005640.html

Fix:

- Modify `print_supported_protocols` and `print_supported_protocols_usage` to
  rely on the length returned by `openconnect_get_supported_protocols`.
- Restore the sentinel value at the end of the array returned by
  `openconnect_get_supported_protocols`, to preserve ABI compatibility for
  other users who may depend on this sentinel.

Signed-off-by: Daniel Lenski <dlenski@gmail.com>

library.c
@@ -195,7 +195,13 @@ int openconnect_get_supported_protocols(struct oc_vpn_proto **protos)
 	struct oc_vpn_proto *pr;
 	int i;
 
-	*protos = pr = calloc(NR_PROTOS, sizeof(*pr));
+        /* The original version of this function included an all-zero
+         * sentinel value at the end of the array, so we must continue
+         * to do so for ABI compatibility even though it's
+         * functionally redundant as a marker of the array's length,
+         * along with the explicit length in the return value.
+         */
+	*protos = pr = calloc(NR_PROTOS + 1, sizeof(*pr));
 	if (!pr)
 		return -ENOMEM;
 

main.c
@@ -659,10 +659,12 @@ static void print_supported_protocols(void)
 {
 	const char *comma = ", ", *sep = comma + 1;
 	struct oc_vpn_proto *protos, *p;
+	int n;
 
-	if (openconnect_get_supported_protocols(&protos)>=0) {
+	n = openconnect_get_supported_protocols(&protos);
+        if (n>=0) {
 		printf(_("Supported protocols:"));
-		for (p=protos; p->name; p++) {
+		for (p=protos; n; p++, n--) {
 			printf("%s%s%s", sep, p->name, p==protos ? _(" (default)") : "");
 			sep = comma;
 		}
@@ -674,10 +676,12 @@ static void print_supported_protocols(void)
 static void print_supported_protocols_usage(void)
 {
 	struct oc_vpn_proto *protos, *p;
+	int n;
 
-	if (openconnect_get_supported_protocols(&protos)>=0) {
+	n = openconnect_get_supported_protocols(&protos);
+        if (n>=0) {
 		printf("\n%s:\n", _("Set VPN protocol"));
-		for (p=protos; p->name; p++)
+		for (p=protos; n; p++, n--)
 			printf("      --protocol=%-16s %s%s\n",
 				   p->name, p->description, p==protos ? _(" (default)") : "");
 		openconnect_free_supported_protocols(protos);