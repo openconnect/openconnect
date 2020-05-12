0acdb997951f4a50090b482de56c7b357716e2ad
David Woodhouse <dwmw2@infradead.org>
2020-05-12 20:41:06 +0100

    Add FCS support
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- ppp.c
+++ ppp.c
@@ -39,12 +39,53 @@
 #define ECHOREP 10
 #define DISCREQ 11
 
+
+static const uint16_t fcstab[256] = {
+	0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf,
+	0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7,
+	0x1081, 0x0108, 0x3393, 0x221a, 0x56a5, 0x472c, 0x75b7, 0x643e,
+	0x9cc9, 0x8d40, 0xbfdb, 0xae52, 0xdaed, 0xcb64, 0xf9ff, 0xe876,
+	0x2102, 0x308b, 0x0210, 0x1399, 0x6726, 0x76af, 0x4434, 0x55bd,
+	0xad4a, 0xbcc3, 0x8e58, 0x9fd1, 0xeb6e, 0xfae7, 0xc87c, 0xd9f5,
+	0x3183, 0x200a, 0x1291, 0x0318, 0x77a7, 0x662e, 0x54b5, 0x453c,
+	0xbdcb, 0xac42, 0x9ed9, 0x8f50, 0xfbef, 0xea66, 0xd8fd, 0xc974,
+	0x4204, 0x538d, 0x6116, 0x709f, 0x0420, 0x15a9, 0x2732, 0x36bb,
+	0xce4c, 0xdfc5, 0xed5e, 0xfcd7, 0x8868, 0x99e1, 0xab7a, 0xbaf3,
+	0x5285, 0x430c, 0x7197, 0x601e, 0x14a1, 0x0528, 0x37b3, 0x263a,
+	0xdecd, 0xcf44, 0xfddf, 0xec56, 0x98e9, 0x8960, 0xbbfb, 0xaa72,
+	0x6306, 0x728f, 0x4014, 0x519d, 0x2522, 0x34ab, 0x0630, 0x17b9,
+	0xef4e, 0xfec7, 0xcc5c, 0xddd5, 0xa96a, 0xb8e3, 0x8a78, 0x9bf1,
+	0x7387, 0x620e, 0x5095, 0x411c, 0x35a3, 0x242a, 0x16b1, 0x0738,
+	0xffcf, 0xee46, 0xdcdd, 0xcd54, 0xb9eb, 0xa862, 0x9af9, 0x8b70,
+	0x8408, 0x9581, 0xa71a, 0xb693, 0xc22c, 0xd3a5, 0xe13e, 0xf0b7,
+	0x0840, 0x19c9, 0x2b52, 0x3adb, 0x4e64, 0x5fed, 0x6d76, 0x7cff,
+	0x9489, 0x8500, 0xb79b, 0xa612, 0xd2ad, 0xc324, 0xf1bf, 0xe036,
+	0x18c1, 0x0948, 0x3bd3, 0x2a5a, 0x5ee5, 0x4f6c, 0x7df7, 0x6c7e,
+	0xa50a, 0xb483, 0x8618, 0x9791, 0xe32e, 0xf2a7, 0xc03c, 0xd1b5,
+	0x2942, 0x38cb, 0x0a50, 0x1bd9, 0x6f66, 0x7eef, 0x4c74, 0x5dfd,
+	0xb58b, 0xa402, 0x9699, 0x8710, 0xf3af, 0xe226, 0xd0bd, 0xc134,
+	0x39c3, 0x284a, 0x1ad1, 0x0b58, 0x7fe7, 0x6e6e, 0x5cf5, 0x4d7c,
+	0xc60c, 0xd785, 0xe51e, 0xf497, 0x8028, 0x91a1, 0xa33a, 0xb2b3,
+	0x4a44, 0x5bcd, 0x6956, 0x78df, 0x0c60, 0x1de9, 0x2f72, 0x3efb,
+	0xd68d, 0xc704, 0xf59f, 0xe416, 0x90a9, 0x8120, 0xb3bb, 0xa232,
+	0x5ac5, 0x4b4c, 0x79d7, 0x685e, 0x1ce1, 0x0d68, 0x3ff3, 0x2e7a,
+	0xe70e, 0xf687, 0xc41c, 0xd595, 0xa12a, 0xb0a3, 0x8238, 0x93b1,
+	0x6b46, 0x7acf, 0x4854, 0x59dd, 0x2d62, 0x3ceb, 0x0e70, 0x1ff9,
+	0xf78f, 0xe606, 0xd49d, 0xc514, 0xb1ab, 0xa022, 0x92b9, 0x8330,
+	0x7bc7, 0x6a4e, 0x58d5, 0x495c, 0x3de3, 0x2c6a, 0x1ef1, 0x0f78
+};
+
+#define PPPINITFCS16    0xffff  /* Initial FCS value */
+#define PPPGOODFCS16    0xf0b8  /* Good final FCS value */
+
+#define foldfcs(fcs, c) (  ( (fcs) >> 8 ) ^ fcstab[(fcs ^ (c)) & 0xff] )
+
 const char *lcp_names[] = {NULL, "Configure-Request", "Configure-Ack", "Configure-Nak", "Configure-Reject", "Terminate-Request",
 			   "Terminate-Ack", "Code-Reject", "Protocol-Reject", "Echo-Request", "Echo-Reply", "Discard-Request"};
 
 #define ASYNCMAP_LCP 0xffffffffUL
 
-#define NEED_ESCAPE(c, map) ( ((c < 0x20) && (map && (1UL << (c)))) || (c == 0x7d) || (c == 0x7e) )
+#define NEED_ESCAPE(c, map) ( (((c) < 0x20) && (map && (1UL << (c)))) || ((c) == 0x7d) || ((c) == 0x7e) )
 
 #define HDLC_ZERO_FCS 1
 #define HDLC_REAL_FCS 2
@@ -53,13 +94,16 @@ static struct pkt *hdlc_into_new_pkt(struct openconnect_info *vpninfo, unsigned 
 {
         const unsigned char *inp = bytes, *endp = bytes + len;
 	unsigned char *outp;
-	struct pkt *p = malloc(sizeof(struct pkt) + len*2 + 4);
+	uint16_t fcs = PPPINITFCS16;
+	struct pkt *p = malloc(sizeof(struct pkt) + len*2 + 6);
 	if (!p)
 		return NULL;
-	outp = p->data;
 
+	outp = p->data;
 	*outp++ = 0x7e;
+
 	for (; inp < endp; inp++) {
+		fcs = foldfcs(fcs, *inp);
 		if (NEED_ESCAPE(*inp, asyncmap)) {
 			*outp++ = 0x7d;
 			*outp++ = *inp ^ 0x20;
@@ -67,9 +111,20 @@ static struct pkt *hdlc_into_new_pkt(struct openconnect_info *vpninfo, unsigned 
 			*outp++ = *inp;
 	}
 
-	/* XX: need real FCS */
-	*outp++ = 0;
-	*outp++ = 0;
+	/* Append FCS, escaped, little-endian */
+	fcs ^= 0xffff;
+	if (NEED_ESCAPE(fcs & 0xff, asyncmap)) {
+		*outp++ = 0x7d;
+		*outp++ = (fcs & 0xff) ^ 0x20;
+	} else
+		*outp++ = fcs & 0xff;
+
+	fcs >>= 8;
+	if (NEED_ESCAPE(fcs, asyncmap)) {
+		*outp++ = 0x7d;
+		*outp++ = fcs ^ 0x20;
+	} else
+		*outp++ = fcs;
 
 	*outp++ = 0x7e;
 	p->ppp.hlen = 0;
@@ -79,10 +134,9 @@ static struct pkt *hdlc_into_new_pkt(struct openconnect_info *vpninfo, unsigned 
 
 static int unhdlc_in_place(struct openconnect_info *vpninfo, unsigned char *bytes, int len, unsigned char **next)
 {
-	const unsigned char *inp = bytes, *endp = bytes + len;
+	unsigned char *inp = bytes, *endp = bytes + len;
 	unsigned char *outp = bytes;
-	int escape = 0;
-	uint16_t fcs;
+	uint16_t fcs = PPPINITFCS16;
 
 	if (*inp == 0x7e)
 		inp++;
@@ -90,38 +144,50 @@ static int unhdlc_in_place(struct openconnect_info *vpninfo, unsigned char *byte
 		vpn_progress(vpninfo, PRG_DEBUG,
 			     _("HDLC initial flag sequence (0x7e) is missing\n"));
 
-	for (; *inp != 0x7e && inp < endp; inp++) {
-		if (*inp == 0x7d)
-			escape = 1;
-		else if (escape) {
-			*outp++ = *inp ^ 0x20;
-			escape = 0;
-		} else
-			*outp++ = *inp;
+	for (; inp < endp; inp++) {
+		unsigned char c = *inp;
+		if (c == 0x7e)
+			goto done;
+		else if (c == 0x7d) {
+			if (++inp == endp) {
+				vpn_progress(vpninfo, PRG_DEBUG,
+                                            _("HDLC packet ended with dangling escape.\n"));
+				return -EINVAL;
+
+			}
+			c = *inp ^ 0x20;
+		}
+		fcs = foldfcs(fcs, c);
+		*outp++ = c;
 	}
-	if (*inp++ != 0x7e) {
+	vpn_progress(vpninfo, PRG_ERR,
+		     _("HDLC buffer ended without FCS and flag sequence (0x7e)\n"));
+	return -EINVAL;
+
+ done:
+	if (outp < bytes + 2) {
 		vpn_progress(vpninfo, PRG_ERR,
-			     _("HDLC buffer ended without flag sequence (0x7e)\n"));
-		return -EINVAL;
-	}
-	if (escape) {
-		vpn_progress(vpninfo, PRG_DEBUG, _("HDLC packet ended with dangling escape.\n"));
+			     _("HDLC frame too short (%d bytes)\n"),
+			     (int)(outp - bytes));
 		return -EINVAL;
 	}
 
-	/* Last two decoded bytes are FCS */
-	outp -= 2;
-	fcs = load_be16(outp);
+	outp -= 2; /* FCS */
 
-	/* XX: check FCS */
-	vpn_progress(vpninfo, PRG_TRACE,
-		     _("Un-HDLC'ed packet (%d bytes -> %ld), FCS=0x%04x\n"),
-		     len, outp - bytes, fcs);
+	if (next)
+		*next = inp; /* Pointing at the 0x7e which marks the start of next frame */
 
-	/* Save pointer to remaining data, and return size after decoding */
-	if (next) *next = inp;
-
-	return outp - bytes;
+	if (fcs != PPPGOODFCS16) {
+		vpn_progress(vpninfo, PRG_INFO,
+			     _("Bad HDLC packet FCS %04x\n"), fcs);
+		dump_buf_hex(vpninfo, PRG_INFO, '<', bytes, len);
+		return -EINVAL;
+	} else {
+		vpn_progress(vpninfo, PRG_TRACE,
+			     _("Un-HDLC'ed packet (%d bytes -> %ld), FCS=0x%04x\n"),
+			     len, outp - bytes, fcs);
+		return outp - bytes;
+	}
 }
 
 #define ACCOMP 1
